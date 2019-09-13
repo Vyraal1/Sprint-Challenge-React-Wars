e8e39bfb7ffddbd0e10d4d7caa4744a18dff3e4a
Vyraal1 <kevin.afable@gmail.com>
2019-09-13 11:44:46 -0400

    added axios request and logged data

--- starwars/src/components/CardGrid.js
+++ starwars/src/components/CardGrid.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
+import axios from "axios";
 
-const CardGrid = props => {
-  return null;
-};
+export default function CardGrid() {
+  const [people, setPeople] = useState([]);
 
-export default CardGrid;
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(res => {
+        console.log("This is the response", res);
+        const data = res.data.results;
+        console.log("This is the array of people", data);
+        setPeople(data);
+      })
+      .catch(e => {
+        console.log("Couldn't get data due to", e);
+      });
+  }, []);
+  console.log(people);
+  return <div className="card-container"></div>;
+}
